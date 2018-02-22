815387c583deba1926fcbbbd53f44204cb506252
RFLewandowski <R.F.Lewandowski@gmail.com>
2018-02-22 21:49:11 +0100

    First commit

---
+++ src/main/java/com/example/projectmeetingplanner/ProjectmeetingplannerApplication.java
@@ -0,0 +1,12 @@
+package com.example.projectmeetingplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectmeetingplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectmeetingplannerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/projectmeetingplanner/ProjectmeetingplannerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.projectmeetingplanner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectmeetingplannerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
